989b5812ca9eb6d3d8aeda4bbfb7c10504b1d151
John Edelbi <iceflaming3@gmail.com>
2023-10-27 14:03:43 +0200

    create simple productControllers

---
+++ backend/controllers/product.js
@@ -0,0 +1,9 @@
+const productControllers = {
+    getProducts: (req, res) => {},
+    getProductByCategory: (req, res) => {},
+    addProducts: (req, res) => {},
+    updateProducts: (req, res) => {},
+    deleteProducts: (req, res) => {}
+};
+
+export default productControllers;
