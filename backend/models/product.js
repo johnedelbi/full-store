a6bbb59ba523f477f377f4c933623f834b26c73a
John Edelbi <iceflaming3@gmail.com>
2023-10-27 13:38:01 +0200

    create product model

---
+++ backend/models/product.js
@@ -0,0 +1,35 @@
+import { v4 as newId } from 'uuid';
+
+const products = [
+    {
+        id: '1',
+        category: 'electronic',
+        name: 'laptop',
+        price: '500',
+        img: 'https://cdn.thewirecutter.com/wp-content/media/2023/06/bestlaptops-2048px-9765.jpg'
+    }
+];
+
+class Product {
+    constructor(category, name, price, img) {
+        this.id = newId();
+        this.category = category;
+        this.name = name;
+        this.price = price;
+        this.img = img;
+    }
+
+    static getProducts = () => {
+        return products;
+    };
+
+    static getProductByCategory = (category) => {
+        return products.filter((product) => product.category === category);
+    };
+
+    addProduct = () => {
+        products.push(this);
+    };
+}
+
+export default Product;
