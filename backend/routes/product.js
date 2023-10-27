8498a722e3168cca2bbe1cc91f8c711ebb78aac7
John Edelbi <iceflaming3@gmail.com>
2023-10-27 16:01:35 +0200

    add verifyToken middleware to user routes needed

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,14 @@
 import express from 'express';
+
 import productControllers from '../controllers/product.js';
+import verifyToken from '../middleware/verifyToken.js';
+
 const productRoutes = express.Router();
 
 productRoutes.get('/', productControllers.getProducts);
 productRoutes.get('/cat/:category', productControllers.getProductByCategory);
-productRoutes.post('/', productControllers.addProducts);
-productRoutes.put('/:id', productControllers.updateProducts);
-productRoutes.delete('/:id', productControllers.deleteProducts);
+productRoutes.post('/', verifyToken, productControllers.addProducts);
+productRoutes.put('/:id', verifyToken, productControllers.updateProducts);
+productRoutes.delete('/:id', verifyToken, productControllers.deleteProducts);
 
 export default productRoutes;
